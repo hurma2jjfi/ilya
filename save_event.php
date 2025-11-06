f5518153a48adeef364bea236290a933e74b558e
hurmanoskill <149786424+hurma2jjfi@users.noreply.github.com>
2025-11-06 12:39:26 +0400

    Добавлены проверки на уже существующие лоты

--- save_event.php
+++ save_event.php
@@ -32,26 +32,32 @@ $eventTime = $_POST['eventTime'] ?? '';
 $address = $_POST['address'] ?? '';
 $createdAt = $_POST['createdAt'] ?? date('Y-m-d H:i:s');
 
-// Проверка всех обязательных полей
+
 if (!$fio || !$email || !$phone || !$eventType || !$eventDate || !$eventTime || !$address) {
     echo json_encode(["error" => "Заполните все поля формы"]);
     exit;
 }
 
-// Проверка времени между заказами
-$sqlLast = "SELECT created_at FROM orders ORDER BY created_at DESC LIMIT 1";
-$stmtLast = $pdo->query($sqlLast);
-$lastOrder = $stmtLast->fetch(PDO::FETCH_ASSOC);
 
-// if ($lastOrder) {
-//     $lastCreatedAt = strtotime($lastOrder['created_at']);
-//     $currentCreatedAt = strtotime($createdAt);
-//     $diffSeconds = $currentCreatedAt - $lastCreatedAt;
-//     if ($diffSeconds < 4 * 3600) {
-//         echo json_encode(["error" => "Нельзя добавить заказ: с момента последнего заказа прошло меньше 4 часов."]);
-//         exit;
-//     }
-// }
+$currentDate = date('Y-m-d');
+if ($eventDate < $currentDate) {
+    echo json_encode(["error" => "Нельзя выбрать дату в прошлом"]);
+    exit;
+}
+
+
+$sqlCheckSlot = "SELECT COUNT(*) as count FROM orders WHERE event_date = :event_date AND event_time = :event_time";
+$stmtCheckSlot = $pdo->prepare($sqlCheckSlot);
+$stmtCheckSlot->execute([
+    ':event_date' => $eventDate,
+    ':event_time' => $eventTime
+]);
+$result = $stmtCheckSlot->fetch(PDO::FETCH_ASSOC);
+
+if ($result['count'] > 0) {
+    echo json_encode(["error" => "Выбранный вами слот доставки уже занят. Пожалуйста, выберите другой."]);
+    exit;
+}
 
 $sql = "INSERT INTO orders (fio, email, phone, event_type, event_date, event_time, address, created_at) VALUES (:fio, :email, :phone, :event_type, :event_date, :event_time, :address, :created_at)";
 $stmt = $pdo->prepare($sql);
